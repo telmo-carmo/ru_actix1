9e6760d6c1e5ef795677384fb81d694de82c06e3
Telmo Carmo <telmo.carmo@gmail.com>
2025-05-29 15:07:41 +0100

    new gen ssl cert localhost

--- g1.sh
+++ g1.sh
@@ -1,2 +1,8 @@
-openssl req -x509 -newkey rsa:4096 -nodes -sha256 -out cert.pem -keyout key.pem -days 3650 \
+#openssl req -x509 -newkey rsa:4096 -nodes -sha256 -out cert.pem -keyout key.pem -days 3650 \
   -subj "/C=PT/ST=Lisboa/L=Lisboa/O=NB/OU=DSI/CN=localhost"  -addext "subjectAltName=DNS:localhost"
+
+  # run interactive mode:
+  openssl req -x509 -newkey rsa:4096 -nodes -sha256 -out cert.pem -keyout key.pem -days 3650   -addext "subjectAltName=DNS:localhost"
+
+  openssl x509 -in cert.pem -text -noout 
+  
